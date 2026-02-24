c592c92cd5d2932c3df1d2f589a5f448621c699f
Rodrigo720 <rodrigotacuri_99@hotmail.it>
2026-02-24 22:14:59 +0100

    aggiunta di gitignore

--- data/src/ingestion/extract_match_ids.py
+++ data/src/ingestion/extract_match_ids.py
@@ -14,10 +14,28 @@ def extract_match_ids():
         with file.open('r',encoding='utf-8') as f:
             partite=json.load(f)
 
+            print(f"{file.name}: {len(partite)} matches")
+
             #per ogni partita dentro partite, prendo il suo match_id e mettilo in lista
             new_list = [p["match_id"] for p in partite]
-            #accumula nella lista principale
+            #accumula nella lista principale senza duplicare
             tutti_id.update(new_list)
+
+    print(f"\nTotale match_id unici: {len(tutti_id)}")
+
+    return list(tutti_id)
+
+
+
+if __name__ == "__main__":
+    match_ids = extract_match_ids()
+    output_path= Path("data/processed/match_ids.json")
+    output_path.parent.mkdir(parents=True,exist_ok=True)
+
+    with output_path.open('w',encoding='utf-8') as f:
+        json.dump(match_ids,f,ensure_ascii=False,indent=2)
+    print(f"\nSalvato file: {output_path}")
+
     
 
 
