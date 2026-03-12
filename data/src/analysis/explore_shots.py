aba8ce979cd8c308f4b4c4407a91889e29e49b3f
Rodrigo720 <rodrigotacuri_99@hotmail.it>
2026-03-12 22:21:25 +0100

    modifiche effettuate

---
+++ data/src/analysis/explore_shots.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns 
+from pathlib import Path
+
+INPUT_PATH = Path("data/processed/shots_model_ready_wc2022.csv")
+FIGURES_PATH = Path("report/figures")
+
+def main():
+    FIGURES_PATH.mkdir(parents=True,exist_ok=True)
+
+    df = pd.read_csv(INPUT_PATH)
+
+    print("Dataset shape:", df.shape)
+    print(df["goal"].value_counts())
+
+    sns.set_theme(style="whitegrid")
+
+    #distribuzione goal/no goal
+    plt.figure(figsize=(6,4))
+    sns.countplot(x="goal",data=df)
+    plt.title("Distribution of Goals vs Non Goals")
+    plt.xlabel("Goal")
+    plt.ylabel("Number of shots")
+    plt.savefig(FIGURES_PATH/"goal_distribution_wc2022.png")
+    plt.close()
+
+    #distribuzione distanza tiro
+    plt.figure(figsize=(8,5))
+    sns.histplot(df["distance"],bins=30, kde=True)
+    plt.title("Distribution of shot distance")
+    plt.xlabel("distance from goal")
+    plt.ylabel("frequency")
+    plt.savefig(FIGURES_PATH/"distance_distribution_wc_2022.png")
+    plt.close()
+
+    #Goal rate per distanza
+    df["distance_bin"] = pd.cut(df["distance"],bins=10)
+
+    goal_rate = df.groupby("distance_bin")["goal"].mean().reset_index() 
+
+    plt.figure(figsize=(8,5))
+    sns.barplot(x="distance_bin", y="goal", data=goal_rate)
+    plt.xticks(rotation=45)
+    plt.title("Goal Rate by Distance")
+    plt.xlabel("Distance bin")
+    plt.ylabel("Goal probability")
+    plt.savefig(FIGURES_PATH/"goal_rate_by_distance_wc2022.png")
+    plt.close
+
+    #Distribuzione body part
+    plt.figure(figsize=(8,5))
+    sns.countplot(y="shot.body_part.name", data=df)
+    plt.title("Shot Distribution by Body Part")
+    plt.xlabel("Number of Shots")
+    plt.ylabel("Body Part")
+    plt.savefig(FIGURES_PATH / "body_part_distribution_wc2022.png")
+    plt.close()
+
+    #Distribuzione shot type
+    plt.figure(figsize=(8,5))
+    sns.countplot(y="shot.type.name", data=df)
+    plt.title("Shot Distribution by Shot Type")
+    plt.xlabel("Number of Shots")
+    plt.ylabel("Shot Type")
+    plt.savefig(FIGURES_PATH / "shot_type_distribution_wc2022.png")
+    plt.close()
+
+    #Under pressure vs goal rate
+    pressure_goal = df.groupby("under_pressure")["goal"].mean().reset_index()
+
+    plt.figure(figsize=(6,4))
+    sns.barplot(x="under_pressure", y="goal", data=pressure_goal)
+    plt.title("Goal Rate Under Pressure")
+    plt.xlabel("Under Pressure")
+    plt.ylabel("Goal Probability")
+    plt.savefig(FIGURES_PATH / "goal_rate_under_pressure_wc2022.png")
+    plt.close()
+
+    #Play pattern distribution
+    plt.figure(figsize=(8,5))
+    sns.countplot(y="play_pattern.name", data=df)
+    plt.title("Shot Distribution by Play Pattern")
+    plt.xlabel("Number of Shots")
+    plt.ylabel("Play Pattern")
+    plt.savefig(FIGURES_PATH / "play_pattern_distribution_wc2022.png")
+    plt.close()
+
+    print("EDA completed. Figures saved in reports/figures/")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
---
+++ data/src/processing/build_model_dataset.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+
+INPUT_PATH = Path("data/processed/shots_raw_full_wc2022.csv")
+OUTPUT_PATH = Path("data/processed/shots_model_ready_wc2022.csv")
+
+
+def calculate_distance(x, y):
+
+    goal_x = 120
+    goal_y = 40
+
+    return np.sqrt((goal_x - x) ** 2 + (goal_y - y) ** 2)
+
+
+def calculate_angle(x, y):
+
+    goal_width = 7.32
+    goal_x = 120
+
+    left_post_y = 40 + goal_width / 2
+    right_post_y = 40 - goal_width / 2
+
+    angle = np.arctan2(left_post_y - y, goal_x - x) - np.arctan2(
+        right_post_y - y, goal_x - x
+    )
+
+    return np.abs(angle)
+
+
+if __name__ == "__main__":
+
+    df = pd.read_csv(INPUT_PATH)
+
+    # target variable 
+    df["goal"] = (df["shot.outcome.name"] == "Goal").astype(int)
+
+    # estrazione coordinate
+    coords = df["location"].str.strip("[]").str.split(",", expand=True)
+
+    df["x"] = coords[0].astype(float)
+    df["y"] = coords[1].astype(float)
+
+    # feature engineering
+    df["distance"] = calculate_distance(df["x"], df["y"])
+    df["angle"] = calculate_angle(df["x"], df["y"])
+
+    # selezione colonne utili
+    columns = [
+        "match_id",
+        "team.name",
+        "minute",
+        "possession",
+        "play_pattern.name",
+        "position.name",
+        "under_pressure",
+        "shot.type.name",
+        "shot.technique.name",
+        "shot.body_part.name",
+        "x",
+        "y",
+        "distance",
+        "angle",
+        "goal",
+        "shot.statsbomb_xg",
+    ]
+
+    model_df = df[columns].copy()
+
+    OUTPUT_PATH.parent.mkdir(parents=True, exist_ok=True)
+    model_df.to_csv(OUTPUT_PATH, index=False)
+
+    print("Dataset per modello creato")
+    print(f"Righe: {len(model_df)}")
+    print(f"Colonne: {len(model_df.columns)}")
